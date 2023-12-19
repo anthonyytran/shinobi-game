5f7fc6801cd1f59d1ae4b5b9ae01c6fc696b3fd4
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-12-19 22:12:39 +1100

    Added message history

--- engine.py
+++ engine.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
-from render_functions import render_bar
-
+from message_log import MessageLog
+from render_functions import render_bar, render_names_at_mouse_location
 from typing import TYPE_CHECKING
 
 from tcod.context import Context
@@ -20,6 +20,8 @@ class Engine:
 
     def __init__(self, player: Actor):
         self.event_handler: EventHandler = MainGameEventHandler(self)
+        self.message_log = MessageLog()
+        self.mouse_location = (0, 0)
         self.player = player
 
     def handle_enemy_turns(self) -> None:
@@ -37,8 +39,9 @@ class Engine:
         # If a tile is "visible" it should be added to "explored".
         self.game_map.explored |= self.game_map.visible
 
-    def render(self, console: Console, context: Context) -> None:
+    def render(self, console: Console) -> None:
         self.game_map.render(console)
+        self.message_log.render(console=console, x=21, y=45, width=40, height=5)
 
         render_bar(
             console=console,
@@ -47,6 +50,4 @@ class Engine:
             total_width=20,
         )
 
-        context.present(console)
-
-        console.clear()
+        render_names_at_mouse_location(console=console, x=21, y=44, engine=self)
