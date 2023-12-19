59997ecc2d0ab1a6d42a10e7f4d6325f1cf5eeec
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-12-19 21:44:44 +1100

    Refactor

--- render_functions.py
+++ render_functions.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import color
+import colour
 
 if TYPE_CHECKING:
     from tcod import Console
@@ -13,13 +13,13 @@ def render_bar(
 ) -> None:
     bar_width = int(float(current_value) / maximum_value * total_width)
 
-    console.draw_rect(x=0, y=45, width=total_width, height=1, ch=1, bg=color.bar_empty)
+    console.draw_rect(x=0, y=45, width=total_width, height=1, ch=1, bg=colour.bar_empty)
 
     if bar_width > 0:
         console.draw_rect(
-            x=0, y=45, width=bar_width, height=1, ch=1, bg=color.bar_filled
+            x=0, y=45, width=bar_width, height=1, ch=1, bg=colour.bar_filled
         )
 
     console.print(
-        x=1, y=45, string=f"HP: {current_value}/{maximum_value}", fg=color.bar_text
+        x=1, y=45, string=f"HP: {current_value}/{maximum_value}", fg=colour.bar_text
     )
