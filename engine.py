726c4090fa41afa67dbd6004b370cf7a7c4055dd
Anthony Tran <anthonytran@Anthonys-MacBook-Air.local>
2023-12-21 14:29:08 +1100

    AI exceptions

--- engine.py
+++ engine.py
@@ -7,6 +7,7 @@ from tcod.context import Context
 from tcod.console import Console
 from tcod.map import compute_fov
 
+import exceptions
 from input_handlers import MainGameEventHandler
 
 if TYPE_CHECKING:
@@ -27,7 +28,10 @@ class Engine:
     def handle_enemy_turns(self) -> None:
         for entity in set(self.game_map.actors) - {self.player}:
             if entity.ai:
-                entity.ai.perform()
+                try:
+                    entity.ai.perform()
+                except exceptions.Impossible:
+                    pass # Ignore impossible action exceptions from AI. 
 
     def update_fov(self) -> None:
         """Recompute the visible area based on the players point of view."""
