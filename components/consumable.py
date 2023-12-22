f747cef0631ec3b302d11d691477534fc5499f1e
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-12-22 16:18:10 +1100

    Fixed lightning bolt

--- components/consumable.py
+++ components/consumable.py
@@ -56,8 +56,8 @@ class LightningDamageConsumable(Consumable):
     def __init__(self, damage: int, maximum_range: int):
         self.damage = damage
         self.maximum_range = maximum_range
-    
-    def activiate(self, action: actions.ItemAction) -> None:
+
+    def activate(self, action: actions.ItemAction) -> None:
         consumer = action.entity
         target = None
         closest_distance = self.maximum_range + 1.0
@@ -69,12 +69,12 @@ class LightningDamageConsumable(Consumable):
                 if distance < closest_distance:
                     target = actor
                     closest_distance = distance
-        
+
         if target:
             self.engine.message_log.add_message(
                 f"A lighting bolt strikes the {target.name} with a loud thunder, for {self.damage} damage!"
             )
             target.fighter.take_damage(self.damage)
             self.consume()
-        else: 
+        else:
             raise Impossible("No enemy is close enough to strike.")
