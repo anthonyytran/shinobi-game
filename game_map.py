0f8c613cfe30bfa28ae4fa8dbf20c1f7ef4eebcb
Anthony Tran <anthonytran@MacBook-Air.localdomain>
2024-01-17 05:12:16 +1100

    downstairs dungeon

--- game_map.py
+++ game_map.py
@@ -29,6 +29,8 @@ class GameMap:
             (width, height), fill_value=False, order="F"
         )  # Tiles the player has seen before
 
+        self.downstairs_location = (0, 0)
+
     @property
     def gamemap(self) -> GameMap:
         return self
